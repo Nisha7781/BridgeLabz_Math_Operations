04835133c9940f7096248b213b2d4fe303906262
Nisha7781 <nishamali0781@gmail.com>
2024-03-26 12:33:12 +0530

    Use Lambda expression to perform math operations

--- src/main/java/org/math/operations/MathCalculator.java
+++ src/main/java/org/math/operations/MathCalculator.java
@@ -5,7 +5,14 @@ public class MathCalculator
 
     public static void main(String[] args)
     {
-        System.out.println("Math Operations Using Lambda");
-    }
+        MathOperations add = (a,b) -> a+b;
+        MathOperations sub = (a,b) -> a-b;
+        MathOperations mul = (a,b) -> a*b;
+        MathOperations div = (a,b) -> a/b;
+
+        System.out.println("Addition is : " + add.calculate(8, 5));
+        System.out.println("Subtraction is : " + sub.calculate(20, 5));
+        System.out.println("Multiplication is : " + mul.calculate(10, 4));
+        System.out.println("Division is : " + div.calculate(18, 3));    }
 
 }
---
+++ src/main/java/org/math/operations/MathOperations.java
@@ -0,0 +1,6 @@
+package org.math.operations;
+
+public interface MathOperations
+{
+    int calculate(int a, int b);
+}

59a835b3a037669a479241b5b25f9d1eece83f9c
Nisha7781 <nishamali0781@gmail.com>
2024-03-26 14:10:06 +0530

    Created a number list and iterate using foreach and print elements

--- src/main/java/org/math/operations/MathCalculator.java
+++ src/main/java/org/math/operations/MathCalculator.java
@@ -1,19 +1,35 @@
 package org.math.operations;
 
-public class MathCalculator
-{
+import java.util.ArrayList;
+import java.util.List;
 
-    public static void main(String[] args)
-    {
+public class MathCalculator {
+
+    public static void main(String[] args) {
         MathOperations add = (a, b) -> a + b;
-        MathOperations sub = (a, b) -> a - b;
+        MathOperations sub = new MathOperations() {
+            @Override
+            public int calculate(int a, int b) {
+                return a - b;
+            }
+        };
         MathOperations mul = (a, b) -> a * b;
         MathOperations div = (a, b) -> a / b;
 
         displayResult("Addition", add, 8, 5);
         displayResult("Subtraction", sub, 20, 5);
-        displayResult("Multiplication", mul, 40, 4);
+        displayResult("Multiplication", mul, 10, 4);
         displayResult("Division", div, 18, 3);
+
+
+        List<Integer> numbers = new ArrayList<>();
+        numbers.add(10);
+        numbers.add(20);
+        numbers.add(30);
+        numbers.add(40);
+
+        System.out.println("\n Iterating through number play list using forEach:");
+        numbers.forEach(System.out::println);
     }
 
     private static void displayResult(String operation, MathOperations M, int a, int b)
@@ -21,3 +37,4 @@ public class MathCalculator
         System.out.println(operation + " is : " + M.calculate(a, b));
     }
 }
+

59c66f804a996feefb2590015b16728ec38c853b
Nisha7781 <nishamali0781@gmail.com>
2024-03-26 16:42:14 +0530

    Transform into double and store using stream.map function

--- src/main/java/org/math/operations/MathCalculator.java
+++ src/main/java/org/math/operations/MathCalculator.java
@@ -4,9 +4,12 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Function;
 import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
-public class MathCalculator {
-    public static void main(String[] args) {
+public class MathCalculator
+{
+    public static void main(String[] args)
+    {
         MathOperations add = (a, b) -> a + b;
         MathOperations sub = (a, b) -> a - b;
         MathOperations mul = (a, b) -> a * b;
@@ -23,19 +26,17 @@ public class MathCalculator {
         numbers.add(25);
         numbers.add(40);
 
-        System.out.println("\nIterating through number playlist and printing only even numbers:");
+        System.out.println("\nTransforming each element to double and storing the result:");
 
-        numbers.stream().forEach(n -> System.out.println("Value: " + n));
+        List<Double> doubles = numbers.stream()
+                .map(Double::valueOf)
+                .collect(Collectors.toList());
 
-        Function<Integer, Double> doubleFunction = Integer::doubleValue;
-        numbers.forEach(n -> {
-            System.out.println("Double Value::" + doubleFunction.apply(n));
-        });
+        System.out.println("Original list:");
+        numbers.forEach(System.out::println);
 
-        Predicate<Integer> isEvenFunction = n -> n % 2 == 0;
-        numbers.forEach(n -> {
-            System.out.println("Value: " + n + " Check for Even: " + isEvenFunction.test(n));
-        });
+        System.out.println("Updated list:");
+        doubles.forEach(System.out::println);
     }
 
     private static void displayResult(String operation, MathOperations M, int a, int b)
@@ -44,4 +45,3 @@ public class MathCalculator {
     }
 
 }
-

d006aa2dc0b32c2adc1a31f0ff5ce92d1ca64d4d
Nisha7781 <nishamali0781@gmail.com>
2024-03-26 12:28:12 +0530

    Master Created

---
+++ src/main/java/org/math/operations/MathCalculator.java
@@ -0,0 +1,11 @@
+package org.math.operations;
+
+public class MathCalculator
+{
+
+    public static void main(String[] args)
+    {
+        System.out.println("Math Operations Using Lambda");
+    }
+
+}
